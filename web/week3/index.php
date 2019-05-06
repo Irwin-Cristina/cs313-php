3373e7875204cf5014c413af404a9f48f149cef9
Bellabot <bellabot@github.com>
2019-05-06 07:29:15 -0600

    include file

--- web/week3/index.php
+++ web/week3/index.php
@@ -6,8 +6,10 @@
     </div>
 
 <h1>Welcome to my home page!</h1>
-<p>Some text.</p>
-<p>Some more text.</p>
+    
+<?php include 'vars.php';
+echo "I have a $color $car.";
+?>
 <?php include 'footer.php';?>
 
 </body>
