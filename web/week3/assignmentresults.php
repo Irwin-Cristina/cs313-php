777e5606f921e8d6f9e9f7799b59cdbd02c99ae4
Bellabot <bellabot@github.com>
2019-05-08 12:12:32 -0600

    team assignment 3

--- web/week3/assignmentresults.php
+++ web/week3/assignmentresults.php
@@ -3,6 +3,12 @@ $name =htmlspecialchars($_POST['name']);
 $email =htmlspecialchars($_POST['email']);
 $major =htmlspecialchars($_POST['major']);
 $comment =htmlspecialchars($_POST['comment']);
+
+if(!empty($_POST["continent"])) {
+    foreach($_POST["continent"] as $check) {
+        echo $check . "<br>";
+    }
+}
 $continent =htmlspecialchars($_POST['continent']);
 ?>
 
