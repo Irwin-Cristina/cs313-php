61284e886574a71b049c7b5ff15432d8366d8a99
Bellabot <bellabot@github.com>
2019-05-11 12:29:16 -0600

    update

--- web/week3/prove/cart.php
+++ web/week3/prove/cart.php
@@ -26,8 +26,8 @@
         if (isset($_SESSION["product"])) {
 
             foreach ($_SESSION["product"] as $item) {
-              for ($i=0; $i < count($_SESSION["product"]); $i++) {
-                //if ($item->name === $product_name) {
+              //for ($i=0; $i < count($_SESSION["product"]); $i++) {
+                if ($item->name === $product_name) {
                     if(($item = array_search($item, $_SESSION["product"])) !==false) {
                     //if(($_SESSION["product"]) !==false) {
                         unset($_SESSION["product"][$item]);
