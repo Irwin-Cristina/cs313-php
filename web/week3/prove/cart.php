024b635546db49bcd4ed6749ff257d264b784757
Bellabot <bellabot@github.com>
2019-05-11 09:13:53 -0600

    update

--- web/week3/prove/cart.php
+++ web/week3/prove/cart.php
@@ -71,7 +71,7 @@ if (isset($_GET["delete"])) {
         <div id="cart_display">
             <div id="name"> <?php echo $games[$i]; ?></div>
             <div id="price"> <?php echo $prices[$i]; ?></div>
-            <a href="cart.php?delete=<?php echo $games[i]; ?>&active=CART">Delete Item</a>
+            <a href="cart.php?delete=<?php echo $games[$i]; ?>&active=CART">Delete Item</a>
         </div>
 <?php
         }
