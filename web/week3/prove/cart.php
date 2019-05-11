2aa497f2cffd8372e2b467cc0c2268a4f177135b
Bellabot <bellabot@github.com>
2019-05-11 12:25:44 -0600

    update

--- web/week3/prove/cart.php
+++ web/week3/prove/cart.php
@@ -23,25 +23,22 @@
         //$product_name=$_GET["delete"];
         $games=$_GET["delete"];
         
-       if (isset($_SESSION["product"])) {
-         //if (isset($_SESSION["product"]) && isset($_SESSION["price"])) {
-           
+        if (isset($_SESSION["product"])) {
+
             foreach ($_SESSION["product"] as $item) {
               for ($i=0; $i < count($_SESSION["product"]); $i++) {
-                
-               //if ($item->name === $product_name) {
-                    //if(($item = array_search($item, $_SESSION["product"])) !==false) {
-                   
-                    if(($_SESSION["product"]) !==false) {
-                        //unset($_SESSION["product"][$item]);
-                        unset($_SESSION["product"]);
+                //if ($item->name === $product_name) {
+                    if(($item = array_search($item, $_SESSION["product"])) !==false) {
+                    //if(($_SESSION["product"]) !==false) {
+                        unset($_SESSION["product"][$item]);
+                        //unset($_SESSION["product"]);
                         break;
                     }
                 }
             }
         }
     
-   }
+    }
      
   ?>
 <!DOCTYPE html>
