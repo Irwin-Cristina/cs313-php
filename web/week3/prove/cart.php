c9357fc45f141590aabddfccfa8572223e07b710
Bellabot <bellabot@github.com>
2019-05-11 09:55:50 -0600

    update

--- web/week3/prove/cart.php
+++ web/week3/prove/cart.php
@@ -59,7 +59,7 @@ if (isset($_GET["delete"])) {
         <div id='nav'>
             <a href='index.php?active=HOME' id="<?php echo ($active==="HOME" ? "active_link" : "non_active_link") ?>">HOME</a>
             <a href='cart.php?active=CART' id="<?php echo ($active==="CART" ? "active_link" : "non_active_link") ?>">CART</a>
-            <a href='index.php?active=CHECKOUT' id="<?php echo ($active==="CHECKOUT" ? "active_link" : "non_active_link") ?>">CHECKOUT</a>
+            <a href='checkout.php' id="<?php echo ($active==="CHECKOUT" ? "active_link" : "non_active_link") ?>">CHECKOUT</a>
         </div>
     </header>     
 <?php
