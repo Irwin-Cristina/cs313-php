8a7f486e84ac2c915630c13cac4f93d93816400e
Bellabot <bellabot@github.com>
2019-05-20 12:58:12 -0600

    testing different queries

--- web/week5/testqueries.php
+++ web/week5/testqueries.php
@@ -1,21 +1,21 @@
 <?php
 
 require_once 'connection.php';
-
+//first option
 foreach ($db->query('SELECT username, password FROM note_user') as $row)
 {
   echo 'user: ' . $row['username'];
   echo ' password: ' . $row['password'];
   echo '<br/>';
 }
-
+//second option
 $statement = $db->query('SELECT username, password FROM note_user');
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
   echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
 }
-
-$statement = $db->query('SELECT username, password FROM note_user');
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+//third option
+//$statement = $db->query('SELECT username, password FROM note_user');
+//$results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
