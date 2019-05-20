fb88aa2b5cb47809a6667398e9816ede595bb8bd
Bellabot <bellabot@github.com>
2019-05-20 12:31:49 -0600

    connection.php

---
+++ web/week5/connection.php
@@ -0,0 +1,37 @@
+<?php
+
+// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
+
+if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+ //$dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
+ $dbUrl = "postgres://tdznrlliupjhom:c8c196fbb7c737cf057037c32c59050d543d92198bea69ae7835817292058be2@ec2-75-101-147-226.compute-1.amazonaws.com:5432/d4hjkl7flc7itv";
+}
+
+$dbopts = parse_url($dbUrl);
+
+print "<p>$dbUrl</p>\n\n";
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+
+try {
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex->getMessage() </p>\n\n";
+ die();
+}
+
+foreach ($db->query('SELECT now()') as $row)
+{
+ print "<p>$row[0]</p>\n\n";
+}
+
+?>
