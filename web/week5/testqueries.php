3485ccd493552af67a56b82292dfd875b3944500
Bellabot <bellabot@github.com>
2019-05-20 13:01:02 -0600

    testing different queries

--- web/week5/testqueries.php
+++ web/week5/testqueries.php
@@ -14,8 +14,20 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
   echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
 }
-//third option
-//$statement = $db->query('SELECT username, password FROM note_user');
-//$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+//third option-does not work???
+$statement = $db->query('SELECT username, password FROM note_user');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+//Using prepared statements
+//option 1
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//option 2
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->execute(array(':name' => $name, ':id' => $id));
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
