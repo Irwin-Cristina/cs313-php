368d7c55330c8be82d59bc7e74cfd570b1b02933
Bellabot <bellabot@github.com>
2019-05-20 12:47:25 -0600

    test queries

---
+++ web/week5/testqueries.php
@@ -0,0 +1,12 @@
+<?php
+
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
+
+
+
+?>
