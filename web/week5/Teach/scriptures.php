81591c01ef5f2488e7fedf5323fb8c34c1d942be
Bellabot <bellabot@github.com>
2019-05-20 19:58:09 -0600

    teach attempt

---
+++ web/week5/Teach/scriptures.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'connection.php';
+
+?>
+
+<!doctype html>
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8">
+        <title>Scriptures-Teach week 5</title>
+        <meta name="author" content="Cristina Irwin">
+    </head>
+    <style>
+        span {
+            font-weight: bold;
+        }
+    
+    </style>
+    <body>
+        <h1>Scripture Resources</h1>
+        
+        <?php
+        foreach ($db->query('SELECT book, chapter, verse, content FROM scripture') as $row)
+            {
+            echo '<p><span>' . $row['book'] . $row['chapter'] . $row['verse'] . '</span>'  .$row['content'] . '</p>';
+            echo '<br/>';
+            }
+        ?>
+    
+    
+    
+    </body>
+
+</html>
