efb0df47285ecdb481ebd8adc3dcb7aa1e6d6cab
Bellabot <bellabot@github.com>
2019-05-20 20:21:33 -0600

    trial again

--- web/week5/Teach/scriptures.php
+++ web/week5/Teach/scriptures.php
@@ -32,6 +32,11 @@ require_once 'connection.php';
             //echo '<p><span>' . $row['book'] . $row['chapter'] . $row['verse'] . '</span>'  .$row['content'] . '</p>';
            // echo '<br/>';
             //}
+        $statement = $db->query('SELECT username, password FROM note_user');
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+            {
+              echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
+            }
         ?>
     
     
