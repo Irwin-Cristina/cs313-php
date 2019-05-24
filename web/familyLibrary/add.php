9b73ccf72c4ffe20be5f2a0389e1aa16478e9e8d
Bellabot <bellabot@github.com>
2019-05-24 12:08:45 -0600

    add connection to add

--- web/familyLibrary/add.php
+++ web/familyLibrary/add.php
@@ -1,4 +1,34 @@
 <?php
+//connection
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+ echo 'user: ' . $row['username'];
+ echo ' password: ' . $row['password'];
+ echo '<br/>';
+}
+
 
     $title = $author = $count = $location = $genre = $summary = '';
     $errors=array('title'=>'', 'author_name'=>'', 'book_page_count'=>'', 'location'=>'','genre_name'=>'', 'book_summary'=>'');
