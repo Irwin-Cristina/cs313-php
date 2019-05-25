53b2289cb478be8bc37183f9da1e6d377cc66cee
Bellabot <bellabot@github.com>
2019-05-24 22:57:18 -0600

    search

--- web/familyLibrary/search-results.php
+++ web/familyLibrary/search-results.php
@@ -1,6 +1,35 @@
 <?php
 //connection
-try
+function get_dbconnection() {
+    $db=NULL;
+
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+
+      $dbOpts = parse_url($dbUrl);
+
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+    return $db;
+
+}
+
+
+/*try
 {
   $dbUrl = getenv('DATABASE_URL');
 
@@ -20,16 +49,16 @@ catch (PDOException $ex)
 {
   echo 'Error!: ' . $ex->getMessage();
   die();
+}*/
+
+foreach ($db->query('SELECT book_title, book_page_count, author_id FROM book INNER JOIN author USING(author_id)') as $row)
+{
+ echo 'Book: ' . $row['book_title'];
+ echo 'Page Count: ' . $row['book_page_count'];
+ echo 'Author: ' . $row['author_id'];
+
+ echo '<br/>';
 }
-
-//foreach ($db->query('SELECT book_title, book_page_count, author_id FROM book INNER JOIN author USING(author_id)') as $row)
-//{
-// echo 'Book: ' . $row['book_title'];
- //echo 'Page Count: ' . $row['book_page_count'];
- //echo 'Author: ' . $row['author_id'];
-
- //echo '<br/>';
-//}
 
 ?>
 <!DOCTYPE html>
@@ -44,9 +73,10 @@ catch (PDOException $ex)
         <div class ="searchcontainer">
         <?php
             if(isset($_POST['submit-search'])) {
-                $search = pg_escape_string($conn, $_POST['search']);
+                $db = get_dbconnection();
+                $search = pg_escape_string($db, $_POST['search']);
                 $sql="SELECT * FROM book WHERE book_title LIKE '%$search%' OR author_id LIKE '%$search%'";
-                $result = pg_query($conn, $sql);
+                $result = pg_query($db, $sql);
                 $queryResult = pg_num_rows($result);
                 
                 if($queryResult > 0) {
