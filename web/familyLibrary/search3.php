86300c7f76789cefd9497d377e696ec353d4e37e
Bellabot <bellabot@github.com>
2019-05-27 13:50:10 -0600

    week video

--- web/familyLibrary/search3.php
+++ web/familyLibrary/search3.php
@@ -1,10 +1,18 @@
 <?php
 
+if (!isset($_GET['author_id'])) 
+{
+    die("Error, course id not specified...");
+}
+$author_id = htmlspecialchars($_GET['author_id']);
+
 require('dbConnect.php');
 $db = get_db();
 
-$query = 'SELECT * FROM author a JOIN book b ON a.author_id = b.author_id;';
+$query = 'SELECT a.author_name, a.author_id FROM author a JOIN book b ON a.author_id = b.author_id WHERE a.author_id=:id;
+';
 $stmt = $db->prepare($query);
+$stmt->bindValue(':id', $author_id, PDO::PARAM_INT);
 $stmt->execute();
 $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -17,15 +25,15 @@ $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <h1>Books</h1>
     <ul>
 <?php
-    var_dump($books);
+    //var_dump($books);
         
     foreach($books as $book){
-        var_dump($book);
+        //var_dump($book);
         
-        $id = $book['book']['book_id'];
-        $title = $book['book.book_title'];
-        $count = $book['book.book_page_count'];
-        $author = $book['author.author_id'];
+        $id = $book['book_id'];
+        $title = $book['book_title'];
+        $count = $book['book_page_count'];
+        $author = $book['author_id'];
         
         echo "<li><p>$title - $author</p></li>";
         echo "<p> $id </p>";
