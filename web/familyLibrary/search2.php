996c9e5f05967f8e4ce36eae389ee2c9d72660cc
Bellabot <bellabot@github.com>
2019-05-27 12:07:07 -0600

    search

--- web/familyLibrary/search2.php
+++ web/familyLibrary/search2.php
@@ -3,7 +3,7 @@
 require('dbConnect.php');
 $db = get_db();
 
-$query = 'SELECT * FROM author a JOIN book b ON a.author_id = b.author_id;';
+$query = 'SELECT book_id, book_title, book_page_count FROM book';
 $stmt = $db->prepare($query);
 $stmt->execute();
 $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -18,13 +18,11 @@ $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <ul>
 <?php
     foreach($books as $book){
-        $id = $book['b.book_id'];
-        $title = $book['b.book_title'];
-        $count = $book['b.book_page_count'];
-        $author = $book['a.author_id'];
+        $id = $book['book_id'];
+        $title = $book['book_title'];
+        $count = $book['book_page_count'];
         
-        echo "<li><p>$title - $author</p></li>";
-        echo "<p> $id </p>";
+        echo "<li><p>$title- $count</p></li>";
         
     }    
         ?>
