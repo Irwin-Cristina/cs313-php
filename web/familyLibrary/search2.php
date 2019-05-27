45c464715690b3893cdd77efe63940a76e44fba6
Bellabot <bellabot@github.com>
2019-05-27 11:47:47 -0600

    search

--- web/familyLibrary/search2.php
+++ web/familyLibrary/search2.php
@@ -3,7 +3,7 @@
 require('dbConnect.php');
 $db = get_db();
 
-    $query = 'SELECT id, code, name FROM course';
+$query = 'SELECT book_id, book_title, book_page_count FROM book';
 $stmt = $db->prepare($query);
 $stmt->execute();
 $courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -11,16 +11,16 @@ $courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Courses</title>
+    <title>Books</title>
 </head>
 <body>
-    <h1>Courses</h1>
+    <h1>Books</h1>
     <ul>
 <?php
-    foreach($courses as $course){
-        $id = $course['id'];
-        $code = $course['code'];
-        $name = $course['name'];
+    foreach($books as $book){
+        $id = $book['book_id'];
+        $code = $book['book_title'];
+        $name = $book['book_page_count'];
         
         echo "<li><p>$code - $name</p></li>";
         
