5638e1c2e362e9c02da1e553376bbab41c98f43f
Bellabot <bellabot@github.com>
2019-05-27 17:50:24 -0600

    teach06

--- web/week6/Teach/scripture.php
+++ web/week6/Teach/scripture.php
@@ -1,7 +1,38 @@
 <?php
 //connection
-require_once 'connection.php';
-$db = get_dbconnection();
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+ echo 'user: ' . $row['username'];
+ echo ' password: ' . $row['password'];
+ echo '<br/>';
+}
+
+
+//require_once 'connection.php';
+//$db = get_dbconnection();
 //SELECT id, chapter, verse, content from scriptures
 
 $query='SELECT id, book, chapter, verse, content FROM scriptures;';
