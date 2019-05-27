8ddc552e2885050ed47c29a67b2d80b47871e8ed
Bellabot <bellabot@github.com>
2019-05-27 14:47:19 -0600

    back to author_id

--- web/familyLibrary/search3.php
+++ web/familyLibrary/search3.php
@@ -1,36 +1,37 @@
 <?php
 
-if (!isset($_GET['book_id'])) 
+if (!isset($_GET['author_id'])) 
 {
     die("Error, course id not specified...");
 }
-$book_id = htmlspecialchars($_GET['book_id']);
+$author_id = htmlspecialchars($_GET['author_id']);
 
 require('dbConnect.php');
 $db = get_db();
 
-$query = 'SELECT * FROM author a JOIN book b ON a.author_id = b.author_id';
+$query = 'SELECT * FROM author a JOIN book b ON a.author_id = b.author_id WHERE a.author_id=:id;
+';
 
 //$query = 'SELECT a.author_name, a.author_id FROM author a JOIN book b ON a.author_id = b.author_id WHERE a.author_id=:id;
 //';
 $stmt = $db->prepare($query);
-//$stmt->bindValue(':id', $author_id, PDO::PARAM_INT);
+$stmt->bindValue(':id', $author_id, PDO::PARAM_INT);
 $stmt->execute();
 $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Book Deatails</title>
+    <title>Books</title>
 </head>
 <body>
-    <h1>Book Details</h1>
+    <h1>Books</h1>
     <ul>
 <?php
     //var_dump($books);
         
     foreach($books as $book){
-       var_dump($book);
+        //var_dump($book);
         
         $id = $book['book_id'];
         $title = $book['book_title'];
