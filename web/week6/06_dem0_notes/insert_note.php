e12bd0c7b6d35d2c8f9f7ac28b861e50de9efd34
Bellabot <bellabot@github.com>
2019-05-27 16:29:13 -0600

    test

--- web/week6/06_dem0_notes/insert_note.php
+++ web/week6/06_dem0_notes/insert_note.php
@@ -5,7 +5,20 @@ $content = htmlspecialchars($_POST['note_content']);
 echo "$course_id\n";
 echo $content;
 
+require('dbconnect.php');
+$db = get_db();
+//SELECT c.code ,c.name, n.content FROM note n JOIN course c ON n.course_id = c.id WHERE c.id = 1;
+$query = 'INSERT INTO note(course_id, content) VALUES (:course_id, :content);';
+$stmt = $db->prepare($query);
+$stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
+$stmt->bindValue(':content', $content, PDO::PARAM_STR);
 
+//$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
 
+$new_page ="course_notes.php?id=$course_id";
+
+header("Location: $new_page");
+die();
 
 ?>
