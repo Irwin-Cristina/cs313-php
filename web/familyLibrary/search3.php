5d10d8e961dc4bcff864dcec8f266f0debea484c
Bellabot <bellabot@github.com>
2019-05-27 14:03:01 -0600

    status

--- web/familyLibrary/search3.php
+++ web/familyLibrary/search3.php
@@ -35,7 +35,7 @@ $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $count = $book['book_page_count'];
         $author = $book['author_id'];
         
-        echo "<p> $id </p>";
+        echo "<li><p>$title - $author</p></li>";
         
     }    
         ?>
