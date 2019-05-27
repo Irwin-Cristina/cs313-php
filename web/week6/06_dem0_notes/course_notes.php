9b38dec869a949e8ecc21809ea86af03ff93aae8
Bellabot <bellabot@github.com>
2019-05-27 13:20:13 -0600

    week video

--- web/week6/06_dem0_notes/course_notes.php
+++ web/week6/06_dem0_notes/course_notes.php
@@ -6,6 +6,25 @@ if (!isset($_GET['course_id']))
 }
 $course_id = htmlspecialchars($_GET['course_id']);
 
+require('dbconnect.php');
+$db = get_db();
+//SELECT c.code ,c.name, n.content FROM note n JOIN course c ON n.course_id = c.id WHERE c.id = 1;
+
+$stmt = $db->prepare('SELECT c.code, c.name, n.content FROM note n JOIN course c ON n.course_id = c.id WHERE c.id=:id
+');
+$stmt->bindValue(':id', $course_id, PDO::PARAM_INT);
+//$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$note_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$course_code = $notes_rows[0]['code'];
+
+$query = 'SELECT id, code, name FROM course';
+$stmt = $db->prepare($query);
+$stmt->execute();
+$courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,8 +32,16 @@ $course_id = htmlspecialchars($_GET['course_id']);
     <title>Course Notes</title>
 </head>
 <body>
-    <h1>Course Notes for course id <?php echo $course_id ?></h1>
-    <p></p>
-
+    <h1>Course Notes for <?php echo $course_code?></h1>
+    <?php
+    foreach ($note_rows as $note_row)
+    {
+        $content = $note_row['content'];
+        echo "<p>$content</p";
+        
+    }
+    
+    
+    ?>
 </body>
 </html>
--- web/week6/06_dem0_notes/courses.php
+++ web/week6/06_dem0_notes/courses.php
@@ -4,7 +4,6 @@ $db = get_db();
 //SELECT id, code, name FROM course;
 //$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
 //$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-//$stmt->bindValue(':name', $name, PDO::PARAM_STR);
 //$stmt->execute();
 //$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
