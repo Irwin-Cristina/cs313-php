224ed1e2ec8e669bfce83c576f2f21ae01b8226c
Bellabot <bellabot@github.com>
2019-05-27 18:22:07 -0600

    teach06

--- web/week6/teach1/scripture.php
+++ web/week6/teach1/scripture.php
@@ -1,34 +1,37 @@
 <?php
 //connection
+//connection
+require_once 'connection1.php';
+$db = get_dbconnection();
 
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-
-foreach ($db->query('SELECT username, password FROM note_user') as $row)
-{
- echo 'user: ' . $row['username'];
- echo ' password: ' . $row['password'];
- echo '<br/>';
-}
+//try
+//{
+//  $dbUrl = getenv('DATABASE_URL');
+//
+//  $dbOpts = parse_url($dbUrl);
+//
+//  $dbHost = $dbOpts["host"];
+//  $dbPort = $dbOpts["port"];
+//  $dbUser = $dbOpts["user"];
+//  $dbPassword = $dbOpts["pass"];
+//  $dbName = ltrim($dbOpts["path"],'/');
+//
+//  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+//
+//  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//}
+//catch (PDOException $ex)
+//{
+//  echo 'Error!: ' . $ex->getMessage();
+//  die();
+//}
+//
+//foreach ($db->query('SELECT username, password FROM note_user') as $row)
+//{
+// echo 'user: ' . $row['username'];
+// echo ' password: ' . $row['password'];
+// echo '<br/>';
+//}
 
 
 //require_once 'connection.php';
