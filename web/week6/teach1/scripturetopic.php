79ab847200921283b7c7e579f8ff104b1c02280d
Bellabot <bellabot@github.com>
2019-05-27 21:33:19 -0600

    teach06

--- web/week6/teach1/scripturetopic.php
+++ web/week6/teach1/scripturetopic.php
@@ -20,6 +20,7 @@ $db = get_dbconnection();
 </style>
 
 <body>
+    <div>
     <h1>Scripture and Topic</h1>
         <?php
         
@@ -30,25 +31,44 @@ $db = get_dbconnection();
             $query = 'SELECT id, book, chapter, verse, content FROM scriptures';
             $stmt = $db->prepare($query);
             $stmt->execute();
-            $scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            //$scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             
-            
-            foreach($scriptures as $scripture)
+            //while ($row = $scriptures)
+            //foreach($scriptures as $scripture)
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
                 {
+                
+                echo '<p>';
+                echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':';
+                echo $row['verse'] . '</strong>' . ' - ' . $row['content'];
+                echo 'Topics: ';
+		      // get the topics now for this scripture    
+                $stmtTopics = $db->prepare('SELECT name FROM topic t'
+			    . ' INNER JOIN scripture_topic st ON st.scripture_id = t.id'
+                . '  WHERE st.scripture_id = :scriptureId');
+                $stmtTopics->bindValue(':scriptureId', $row['id']);
+                $stmtTopics->execute();
+                // Go through each topic in the result
+                
+                    while ($topicRow = $stmtTopics->fetch(PDO::FETCH_ASSOC))
+                    {
+                        echo $topicRow['name'] . ' ';
+                        
+                    echo '</p>';
                //assigns to variable
-                $id = $scripture['id'];
-                $book = $scripture['book'];
-                $chapter = $scripture['chapter'];
-                $verse = $scripture['verse'];
-                $content = $scripture['content'];
+//                $id = $scripture['id'];
+//                $book = $scripture['book'];
+//                $chapter = $scripture['chapter'];
+//                $verse = $scripture['verse'];
+//                $content = $scripture['content'];
 
 //                echo '<p>';
 //                echo '<span>' . $book . ' ' . $chapter . ':';
 //                echo $verse . '</span>' . '--' . $content;
 //                echo '<br>';
 //                echo 'Topics: ';
-                echo "<li><p><a href='scripture_content.php?id=$id'>$book-$chapter-$verse</a></p></li>";
+                //echo "<li><p><a href='scripture_content.php?id=$id'>$book-$chapter-$verse</a></p></li>";
                 }
             
             
@@ -68,6 +88,7 @@ $db = get_dbconnection();
 //        
 //        }
         ?>
+    </div>
     
     <!--<form method="post" action="insert_scripture.php">
         <input type="hidden" name="book_id" value="">
