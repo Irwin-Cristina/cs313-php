b89171456b41bde3d1b23ab65c6f91694146e1ce
Bellabot <bellabot@github.com>
2019-05-27 21:35:56 -0600

    teach06

--- web/week6/teach1/scripturetopic.php
+++ web/week6/teach1/scripturetopic.php
@@ -1,8 +1,36 @@
 <?php
 //connection
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+ echo 'user: ' . $row['username'];
+ echo ' password: ' . $row['password'];
+ echo '<br/>';
+}
 //connection
-require_once ('connection.php');
-$db = get_dbconnection();
+//require_once ('connection.php');
+//$db = get_dbconnection();
 
 
 
