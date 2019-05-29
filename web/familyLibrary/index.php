9c5d78074c0e0ac8d827e46214b0243f6bd27f3c
Bellabot <bellabot@github.com>
2019-05-29 15:27:14 -0600

    index update connection

--- web/familyLibrary/index.php
+++ web/familyLibrary/index.php
@@ -1,35 +1,15 @@
 <?php 
 require('dbConnect.php');
 $db = get_db();
-//require_once 'connection.php';
-//try
-//{
-//  $dbUrl = getenv('DATABASE_URL');
-//
-//  $dbOpts = parse_url($dbUrl);
-//
-//  $dbHost = $dbOpts["host"];
-//  $dbPort = $dbOpts["port"];
-//  $dbUser = $dbOpts["user"];
-//  $dbPassword = $dbOpts["pass"];
-//  $dbName = ltrim($dbOpts["path"],'/');
-//
-//  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-//
-//  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//}
-//catch (PDOException $ex)
-//{
-//  echo 'Error!: ' . $ex->getMessage();
-//  die();
-//}
-//
-//foreach ($db->query('SELECT username, password FROM note_user') as $row)
-//{
-// echo 'user: ' . $row['username'];
-// echo ' password: ' . $row['password'];
-// echo '<br/>';
-//}
+
+foreach ($db->query('SELECT book_title, book_page_count, author_id FROM book') as $row)
+{
+ echo 'Book: ' . $row['book_title'];
+ echo 'Page Count: ' . $row['book_page_count'];
+ echo 'Author: ' . $row['author_id'];
+
+ echo '<br/>';
+}
 
 ?>
 
