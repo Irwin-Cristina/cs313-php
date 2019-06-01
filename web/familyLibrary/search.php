e2c7f3a6a702a8509d784c3756160ee9c6c9037d
Bellabot <bellabot@github.com>
2019-06-01 15:18:52 -0600

    search

--- web/familyLibrary/search.php
+++ web/familyLibrary/search.php
@@ -12,9 +12,9 @@ $output='';
 //
 if(isset($_POST['submit-search'])) { //name from button
     $searchq=$_POST['search'];
-   // $searchq=preg_replace("#[^0-9a-z]#i","",$searchq);
+    $searchq=preg_replace("#[^0-9a-z]#i","",$searchq);
     
-    $query="SELECT * FROM booktemp WHERE book_title LIKE '%$searchq%' OR author LIKE '%$searchq%' OR book_summary LIKE '%$searchq%'";
+    $query="SELECT * FROM booktemp WHERE book_title LIKE '%*$searchq*%' OR author LIKE '%*$searchq*%' OR book_summary LIKE '%*$searchq*%'";
     $stmt = $db->prepare($query);
     $stmt->execute();
     $num_rows = $stmt->fetchColumn();
