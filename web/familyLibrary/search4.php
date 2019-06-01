29fbab48d79ad8b11d42eff270b91b21e1bce537
Bellabot <bellabot@github.com>
2019-06-01 13:18:43 -0600

    search

---
+++ web/familyLibrary/search4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<form action="search4.php" method="post">
+    <input type="text" name="search" placeholder="Search for books" />
+    <input type="submit" value=">>"/>
+    
+    
+</form>
+    
+    
+</body>
+</html>
