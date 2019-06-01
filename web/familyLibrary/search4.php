5aca9a8f59508e93316b99fe1f03103b582cb9b7
Bellabot <bellabot@github.com>
2019-06-01 14:03:59 -0600

    search

--- web/familyLibrary/search4.php
+++ web/familyLibrary/search4.php
@@ -11,11 +11,11 @@ if(isset($_POST['search'])) {
     $searchq=$_POST['search'];
     $searchq=preg_replace("#[^0-9a-z]#i","",$searchq);
     
-    $query="SELECT book_title, book_page_count, book_summary, author FROM booktemp WHERE book_title LIKE '%$searchq%' OR author LIKE '%$searchq%'";
+    $query="SELECT * FROM booktemp WHERE book_title LIKE '%$searchq%' OR author LIKE '%$searchq%'";
     $stmt = $db->prepare($query);
     $stmt->execute();
-    $count = $stmt->fetchColumn();
-    if ($count==0) {
+    $num_rows = $stmt->fetchColumn();
+    if ($num_rows==0) {
         $output = 'There are no search results!';
     }else{
        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
