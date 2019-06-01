e6ee34e321a3aac50d07d0bcdba9975e9228b3ca
Bellabot <bellabot@github.com>
2019-06-01 07:35:42 -0600

    insert_book

--- web/familyLibrary/insert_book.php
+++ web/familyLibrary/insert_book.php
@@ -71,6 +71,48 @@ $db = get_db();
         ?>
         
         </div>
+        
+        <div class="checkbox">
+        <label>Genres:</label><br />
+        
+        <?php
+        //generate checkboxes
+            try {
+                $query = 'SELECT genre_id, genre_name FROM genre';
+                $stmt = $db->prepare($query);
+                //$stmt = $db->prepare('SELECT id, name FROM topic');
+                $stmt->execute();
+                //$topics = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+                //foreach($topics as $topic)
+               {
+                  $id = $row['genre_id'];
+                  $name = $row['genre_name'];
+                  // Notice that we want the value of the checkbox to be the id of the label
+                  echo "<input type='checkbox' name='chkGenres[]' id='chkGenres$id' value='$id'>";
+                  // Also, so they can click on the label, and have it select the checkbox,
+                  // we need to use a label tag, and have it point to the id of the input element.
+                  // The trick here is that we need a unique id for each one. In this case,
+                  // we use "chkTopics" followed by the id, so that it becomes something like
+                  // "chkTopics1" and "chkTopics2", etc.
+                  echo "<label for='chkGenres$id'>$name</label><br />";
+                  // put a newline out there just to make our "view source" experience better
+                  echo "\n";
+
+                    }  
+            }
+            catch (PDOException $ex)
+            {
+               // Please be aware that you don't want to output the Exception message in
+               // a production environment
+           echo "Error connecting to DB. Details: $ex";
+           die();
+            }
+            ?>
+        
+        
+        </div>
 
         <input type="submit" value="Insert scripture">
     </form>
