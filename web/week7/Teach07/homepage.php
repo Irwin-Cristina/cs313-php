2a2507127cc2af51b8f16b1c9f06f92b90634815
Bellabot <bellabot@github.com>
2019-06-03 20:34:51 -0600

    teach07

--- web/week7/Teach07/homepage.php
+++ web/week7/Teach07/homepage.php
@@ -17,7 +17,7 @@ else {
 <body>
     <div>
         <h1>Welcome to the site Homepage!</h1>
-        Your username is: <?= $username ?><br>
+        Your username is: <?php echo $username; ?><br>
         <a href="sign_out.php">Sign Out</a>
                                                 
     
