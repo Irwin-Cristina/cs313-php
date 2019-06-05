5bc971b35ca2d56ca21a610a6fa4ccbec4e40cdc
Bellabot <bellabot@github.com>
2019-06-05 10:43:30 -0600

    search

--- web/familyLibrary/search.php
+++ web/familyLibrary/search.php
@@ -21,19 +21,19 @@ if(isset($_POST['search'])) { //name from button
 
     $stmt = $db->prepare($query) or die("could not search");
     $stmt->execute();
-    //$results=$stmt->fetch(PDO::FETCH_ASSOC);
+    $results=$stmt->fetch(PDO::FETCH_ASSOC);
     
     $num_rows = $stmt->fetchColumn();
     if ($num_rows==0) {
         $output = 'There are no search results!';
     }else{
         
-        foreach ($db->$query as $row)
+        foreach ($results as $result)
        // while ($row = $stmt->fetch($query)) {
       // while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-           $book= $row['book_title'];
-           $author=$row['author'];
-           $count=$row['book_page_count'];
+           $book= $result['book_title'];
+           $author=$result['author'];
+           $count=$result['book_page_count'];
            
            $output.='<div>'.$book. ' Author: ' .$author.' page count: ' .$count.'</div>';
        }
