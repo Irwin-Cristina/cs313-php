3d8bc76d7b69bddd55ba1a969d6b0e151a9077d8
Bellabot <bellabot@github.com>
2019-06-07 13:27:13 -0600

    styling

--- web/familyLibrary/search.php
+++ web/familyLibrary/search.php
@@ -28,7 +28,7 @@ if(isset($_POST['search'])) { //name from button
     if ($num_rows==0) {
         $output = 'There are no search results!';
     }else{
-        foreach ($db->query("SELECT * FROM booktemp WHERE book_title LIKE '%$searchq%'") as $row)
+        foreach ($db->query($query) as $row)
         //foreach ($results as $result)
        // while ($row = $stmt->fetch($query)) {
       // while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
